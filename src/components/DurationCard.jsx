fa49e4489ea61a7d8c7dd0511660ee1ba071c9e9
TanguyChaumette <chaumette.tanguy@gmail.com>
2025-05-27 13:03:09 +0200

    cursor pointer on duration cards

--- src/components/DurationCard.jsx
+++ src/components/DurationCard.jsx
@@ -4,7 +4,7 @@ function DurationCard(props) {
 
   return(
     <>
-      <div className="flex flex-col border-1 border-lightgray aspect-square w-full">
+      <div className="flex flex-col border-1 border-lightgray aspect-square w-full cursor-pointer">
         <h4 className="text-darkgray mt-[16px]">{title}</h4>
         <div className=" flex flex-col mt-[32px] gap-[16px]">
           <h1 className="text-[64px]">{duration}</h1>
